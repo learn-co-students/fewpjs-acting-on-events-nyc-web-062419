85e46f333bb2130b6e9a9ccdf0ee0022af7adee8
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-06 13:20:20 -0400

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+   
+    if (left < 361) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
