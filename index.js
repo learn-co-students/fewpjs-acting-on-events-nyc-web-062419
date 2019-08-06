bdcdc9c53ca31b522adefe81c40d2e735de8be2e
Colin <cstatro@gmail.com>
2019-08-06 13:05:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+const dodger = document.querySelector("div#dodger");
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    console.log("wtf");
+    currentPos = parseInt(dodger.style.left);
+    dodger.style.left = `${currentPos - 1}px`;
+  } else if (e.key === "ArrowRight") {
+    console.log("wtf");
+    rightPos = parseInt(dodger.style.right);
+    dodger.style.left = `${rightPos - 1}px`;
+  }
+});
