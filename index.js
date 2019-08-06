0368a2771cd5003bc46b333bd356694f395e2aa4
yoseflehrer <yolehrer@gmail.com>
2019-08-06 13:58:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 // Your code here
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  } else if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  } else if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  }
+});
+
 function moveDodgerLeft() {
     var leftNumbers = dodger.style.left.replace("px", "");
     var left = parseInt(leftNumbers, 10);
    
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 5.625}px`;
     }
   }
 
-  function moveDodgerRight() {
+function moveDodgerRight() {
+    
+    var rightNumbers = dodger.style.left.replace("px", "")
+    var right = parseInt(rightNumbers, 10)
+    
+    if (right < 360) {
+        dodger.style.left = `${right + 5.625}px`
+    }
+}
+
+function moveDodgerUp() {
       
-      var rightNumbers = dodger.style.left.replace("px", "")
-      var right = parseInt(rightNumbers, 10)
-      
-      if (right < 400) {
-          dodger.style.left = `${right + 1}px`
-      }
+  var upNumbers = dodger.style.bottom.replace("px", "")
+  var up = parseInt(upNumbers, 10)
+  
+  if (up < 380) {
+      dodger.style.bottom = `${up + 5.625}px`
   }
+}
+
+function moveDodgerDown() {
+      
+  var downNumbers = dodger.style.bottom.replace("px", "")
+  var down = parseInt(downNumbers, 10)
+  
+  if (down > 0) {
+      dodger.style.bottom = `${down - 5.625}px`
+  }
+}
